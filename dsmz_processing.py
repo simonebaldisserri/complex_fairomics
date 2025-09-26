abb4bd74a88d9ba49905ddb43b968c1ddad4112e
Simone Baldisserri <simone.baldisserri@studio.unibo.it>
2025-09-26 19:36:32 +0200

    Added ncbi and habitats lists, edits in filling bacteria dict, created matrix with bd_code lists as elements

--- dsmz_processing.py
+++ dsmz_processing.py
@@ -1,26 +1,38 @@
 import numpy as np
 import re
 
-relation_file_path = "/home/selvaggio/Desktop/complex networks/Project/DSMZ_Habitat.txt"
+data_files_path = "/home/selvaggio/Desktop/complex networks/Project/"
 
 #Putting all lines (splitted) in array (of arrays)
-with open(relation_file_path, 'r') as text:
+with open(data_files_path + "DSMZ_Habitat.txt", 'r') as text:
     info = [line.strip().split("\t") for line in text]
 
 bacteria = {}
+all_ncbi = set()
+all_habitats = set()
 
 #Filling bacteria dict
 
 for i in range(len(info)):
     if bacteria.get(info[i][8]):                                                                                    #Verifying if same BD already exists
-        bacteria[info[i][8]][1] += re.split(r'/OBT:|/', info[i][7])[-1]       
+        bacteria[info[i][8]][1] += re.split(r'/OBT:|/', info[i][7])[-1]
+        all_habitats.add(re.split(r'/OBT:|/', info[i][7])[-1])    
     else:
-        bacteria[info[i][8]]=[re.split(r'/ncbi:|/', info[i][3])[1:], re.split(r'/OBT:|/', info[i][7])[-1]]
+        if any(t.startswith("bd:") for t in re.split(r'/ncbi:|/', info[i][3])[1:]):                                               #Deleting bd: specifications in NCBI path
+            bacteria[info[i][8]] = [re.split(r'/ncbi:|/', info[i][3])[1:-1], re.split(r'/OBT:|/', info[i][7])[-1]]
+            all_ncbi.update(re.split(r'/ncbi:|/', info[i][3])[1:-1])
+            all_habitats.add(re.split(r'/OBT:|/', info[i][7])[-1])
+        else:
+            bacteria[info[i][8]]=[re.split(r'/ncbi:|/', info[i][3])[1:], re.split(r'/OBT:|/', info[i][7])[-1]]
+            all_ncbi.update(re.split(r'/ncbi:|/', info[i][3])[1:])
+            all_habitats.add(re.split(r'/OBT:|/', info[i][7])[-1])
 #       Bacterium[BD_CODE] = [[NCBI/PATH/TO/BACTERIUM], MOST_SPECIFIC_HABITAT]
-        if any(t.startswith("bd:") for t in bacteria[info[i][8]][0]):                                               #Deleting bd: specifications in NCBI path
-            bacteria[info[i][8]][0] = [taxid for taxid in bacteria[info[i][8]][0] if not taxid.startswith("bd:")]
 
-with open("/home/selvaggio/Desktop/complex networks/Project/BD_ncbi_hab_id.txt", 'w') as text:
+with open(data_files_path + "BD_ncbi_hab_id.txt", 'w') as text:
     for name, values in list(bacteria.items()):
         ncbi_clean = [' N' + elem for elem in values[0] if elem is not None ] 
         text.write(f"{name}\t{'\t'.join(ncbi_clean)}\tO{values[1]}\n")
+
+
+print(len(all_habitats))
+print(len(all_ncbi))
